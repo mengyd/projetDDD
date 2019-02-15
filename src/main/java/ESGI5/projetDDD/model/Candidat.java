81699a7fcfd2deb2c5716a90b06a9d2686c6f0a0
mengyd <yidongmeng1992@gmail.com>
2019-02-15 14:06:36 +0100

    planifier un entretien

--- src/main/java/ESGI5/projetDDD/model/Candidat.java
+++ src/main/java/ESGI5/projetDDD/model/Candidat.java
@@ -1,5 +1,27 @@
 package ESGI5.projetDDD.model;
 
 public class Candidat {
+	private String nom;
+	private String techno;
+	private int profil;
+	
+	public Candidat(String nom, String techno, int profil) {
+		this.nom = nom;
+		this.techno = techno;
+		this.profil = profil;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public String getTechno() {
+		return techno;
+	}
+
+	public int getProfil() {
+		return profil;
+	}
+	
+	
 }
